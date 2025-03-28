6afa6e2768290f9a5e88a6f719d826dead661129
Gian Caorlin <caorlingiancarlo@gmail.com>
2025-03-28 08:44:47 +0100

    importado de componentes /headder y /footer en en app.component, seteado de rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewComponent } from './pages/new/new.component';
+import { PostComponent } from './pages/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'new', component: NewComponent },
+    { path: 'post/:idPost', component: PostComponent },
+    { path: '**', redirectTo: 'home' }
+];
