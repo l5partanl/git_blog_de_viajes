beb8214ecdc9d03728231152067c9cdfcea8308a
Gian Caorlin <caorlingiancarlo@gmail.com>
2025-04-03 15:34:41 +0200

    (post-entrega) - Unificación de componente Hero dentro de la home.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,48 +1,48 @@
-import { Component, inject } from '@angular/core';
-import { HeroComponent } from "../../components/hero/hero.component";
+import { Component, inject } from "@angular/core";
 import { FeaturedComponent } from "../../components/featured/featured.component";
-import { Post } from '../../interfaces/post.interface';
-import { PostService } from '../../services/post.service';
-import { DatePipe } from '@angular/common';
+import { PostService } from "../../services/post.service";
+import { DatePipe } from "@angular/common";
+import type { Post } from "../../interfaces/post.interface";
 
 @Component({
-  selector: 'app-home',
-  imports: [HeroComponent, FeaturedComponent, DatePipe],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+	selector: "app-home",
+	imports: [/* FeaturedComponent,*/ DatePipe],
+	templateUrl: "./home.component.html",
+	styleUrl: "./home.component.css",
 })
-
 export class HomeComponent {
+	arrPosts: Post[] = [];
 
-  arrPosts: Post[] = [];
+	arrCategories: string[] = [];
 
-  arrCategories: string[] = [];
+	ngAfterViewInit() {
+		this.arrCategories = [
+			...new Set(this.arrPosts.map((post) => post.category)),
+		];
+	}
 
-  ngAfterViewInit() {
-    this.arrCategories = [...new Set(this.arrPosts.map(post => post.category))];
-  }
+	postService = inject(PostService);
 
-  postService = inject(PostService)
+	ngOnInit() {
+		this.loadAllPosts();
+	}
 
-  ngOnInit() {
-    this.loadAllPosts();
-  }
+	loadAllPosts() {
+		const response = this.postService.getAll(); //Recupero del db los datos...
+		this.arrPosts = response; //... y los arrojo al contenedor creado para usarlos en el script
+		console.log(this.arrPosts);
+	}
 
-  loadAllPosts() {
-    const response = this.postService.getAll();//Recupero del db los datos...
-    this.arrPosts = response; //... y los arrojo al contenedor creado para usarlos en el script
-    console.log(this.arrPosts)
-  }
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	onChange($event: any) {
+		if ($event.target.value === "Sin filtros") {
+			this.arrPosts = this.postService.getAll();
+		} else {
+			this.arrPosts = this.postService.getByCat($event.target.value);
+		}
+	}
 
-  onChange($event: any) {
-    if ($event.target.value === "Sin filtros") {
-      this.arrPosts = this.postService.getAll();
-    } else {
-      this.arrPosts = this.postService.getByCat($event.target.value);
-    }
-  }
-
-  redirectToPost(id: number) {
-    window.location.href = `/post/${id}`;
-  }
+	redirectToPost(id: number) {
+		window.location.href = `/post/${id}`;
+	}
 }
